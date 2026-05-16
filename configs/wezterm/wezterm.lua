d38fa2c0d3686b03af6d0001996f627ee9343221
godin <felipegce@outlook.com>
2026-05-16 15:11:10 -0300

    Configurar abrir novas abas e paineis já com o diretório atual

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -17,7 +17,53 @@ end
 -- ====================================
 config.font = wezterm.font 'JetBrainsMono NFM ExtraBold'
 config.color_scheme = 'Vs Code Dark+ (Gogh)'
+-- ====================================
+-- Funcoes
+-- ====================================
+local function split_with_cwd(direction)
+    return wezterm.action_callback(function(window, pane)
+        local cwd = pane:get_current_working_dir()
+        local path = nil
 
+        if cwd then
+            path = cwd.file_path
+            -- Remove barra inicial apenas no Windows
+            if wezterm.target_triple:find('windows') then
+                path = path:gsub('^/', '')
+            end
+        end
+
+        window:perform_action(
+            wezterm.action.SplitPane {
+                direction = direction,
+                command = { cwd = path },
+            },
+            pane
+        )
+    end)
+end
+
+local function spawn_tab_with_cwd()
+    return wezterm.action_callback(function(window, pane)
+        local cwd = pane:get_current_working_dir()
+        local path = nil
+
+        if cwd then
+            path = cwd.file_path
+            -- Remove barra inicial apenas no Windows
+            if wezterm.target_triple:find('windows') then
+                path = path:gsub('^/', '')
+            end
+        end
+
+        window:perform_action(
+            wezterm.action.SpawnCommandInNewTab {
+                cwd = path,
+            },
+            pane
+        )
+    end)
+end
 -- ====================================
 -- Keymaps
 -- ====================================
@@ -25,14 +71,17 @@ config.disable_default_key_bindings = true
 config.leader = { key = 'k', mods = 'ALT', timeout_milliseconds = 2000 }
 config.keys = {
     -- Tabs
-    { key = 't', mods = 'LEADER', action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
+    -- { key = 't', mods = 'LEADER', action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
+    { key = 't', mods = 'LEADER', action = spawn_tab_with_cwd() },
     { key = 'F4', mods = 'LEADER', action = wezterm.action.CloseCurrentTab { confirm = false } },
     { key = 'n', mods = 'LEADER', action = wezterm.action.SpawnWindow },
     { key = '1', mods = 'LEADER', action = act.MoveTabRelative(-1) },
     { key = '2', mods = 'LEADER', action = act.MoveTabRelative(1) },
     -- Paneis
-    { key = ']', mods = 'LEADER', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-    { key = '[', mods = 'LEADER', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+    -- { key = ']', mods = 'LEADER', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+    -- { key = '[', mods = 'LEADER', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { key = ']', mods = 'LEADER', action = split_with_cwd('Right') },
+    { key = '[', mods = 'LEADER', action = split_with_cwd('Down') },
     { key = 'w', mods = 'LEADER', action = wezterm.action.CloseCurrentPane { confirm = false } },
     { key = 'h', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Left' },
     { key = 'l', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Right' },
@@ -46,6 +95,6 @@ config.keys = {
     -- Zoom
     { key = '=', mods = 'LEADER', action = wezterm.action.IncreaseFontSize },
     { key = '-', mods = 'LEADER', action = wezterm.action.DecreaseFontSize },
-    { key = '0', mods = 'LEADER', action = wezterm.action.ResetFontSize },
+    { key = '0', mods = 'LEADER', action = wezterm.action.ResetFontSize }
 }
 return config
